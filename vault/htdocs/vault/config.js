ffa56095c3dbfb9e1a87132960736f839cffd8e0
abhrx <125030565+abhrx@users.noreply.github.com>
2026-04-01 21:38:07 +0600

    Add files via upload

---
+++ vault/htdocs/vault/config.js
@@ -0,0 +1,13 @@
+window.siteConfig = {
+  "defaultServer": "videasy",
+  "maintenance": false,
+  "heroOverride": null,
+  "heroType": "tv",
+  "notification": {
+    "active": false,
+    "title": "Updated",
+    "message": "Version 1.1",
+    "icon": "bell",
+    "duration": 5000
+  }
+};
